c69aecd92ef6ac94780aee9ca5034a210589ac21
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-02-06 21:50:12 +0900

    notify the tasks being moved to Done in the past month

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { createClient, fetchTasks } from "./notion.ts";
+import { IncomingWebhook } from "npm:@slack/webhook";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+const validateEnv = (): void => {
+  const envs = [
+    "NOTION_TOKEN",
+    "NOTION_DATABASE_ID",
+    "SLACK_WEBHOOK_URL",
+  ] as const;
+
+  for (const env of envs) {
+    if (!Deno.env.get(env)) {
+      throw new Error(`${env} is not provided in the environment variables`);
+    }
+  }
+};
+
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  validateEnv();
+
+  const client = createClient();
+  const tasks = await fetchTasks(client);
+
+  const webhook = new IncomingWebhook(Deno.env.get("SLACK_WEBHOOK_URL")!);
+  await Promise.all([
+    webhook.send({ text: JSON.stringify(tasks, null, 2) }),
+    webhook.send({ text: tasks.map((task) => task.url).join("\n") }),
+  ]);
 }
